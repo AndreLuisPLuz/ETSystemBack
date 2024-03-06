80ba4a6fcd17ff45b5aea1e4cd205f20e61943bf
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-06 14:45:47 -0300

    feat: admin basic interface and services added

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,10 +2,12 @@ import { IUserCreatePayload, IUserRegisterPayload } from "./user.interface"
 import { IInstitutionCreatePayload } from "./institution.interface"
 import { IStudentGroupCreatePayload } from "./studentGroup.interface"
 import { IInstructorCreatePayload } from "./instructor.interface"
+import { IAdministratorCreatePayload } from "./administrator.interface"
 
 export {
     IUserCreatePayload, IUserRegisterPayload,
     IInstitutionCreatePayload,
     IStudentGroupCreatePayload,
     IInstructorCreatePayload,
+    IAdministratorCreatePayload,
 }
