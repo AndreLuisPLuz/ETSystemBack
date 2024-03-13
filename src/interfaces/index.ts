6aaf9e8fc4af6aa893d261be87c6a16671654178
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-13 16:55:40 -0300

    feat: retrieveInstructorStudentGroupsService added

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,14 +1,14 @@
 import { IUserCreatePayload, IUserRegisterPayload } from "./user.interface"
 import { IInstitutionCreatePayload } from "./institution.interface"
 import { IStudentGroupCreatePayload } from "./studentGroup.interface"
-import { IInstructorCreatePayload } from "./instructor.interface"
+import { IInstructorCreatePayload, IInstructorStudentGroupsGetResponse } from "./instructor.interface"
 import { IAdministratorCreatePayload } from "./administrator.interface"
 import { ILoginPayload } from "./login.interface"
 
 export {
     IUserCreatePayload, IUserRegisterPayload,
     IInstitutionCreatePayload,
-    IStudentGroupCreatePayload,
+    IStudentGroupCreatePayload, IInstructorStudentGroupsGetResponse,
     IInstructorCreatePayload,
     IAdministratorCreatePayload,
     ILoginPayload,
