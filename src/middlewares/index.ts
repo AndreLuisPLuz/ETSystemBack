ab99120b2edaf7b1c7aa6ec3f05e6ede1380c979
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-25 08:50:33 -0300

    build: deprecated middlewares functions purged.

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,22 +1,9 @@
 import { handleError } from "./handleError.middlewares";
-import { 
-    authenticateToken,
-    authenticateBosch,
-    authenticateAdmin,
-    authenticateMaster,
-    authenticateBoschOrMaster,
-    authenticateOwnUser
-} from "./auth.middleware";
+import { authenticateToken } from "./auth.middleware";
 import { buildRequirements } from "./requirements.middleware";
 
 export {
     handleError,
     authenticateToken,
-    authenticateBosch,
-    authenticateAdmin,
-    authenticateMaster,
-    authenticateBoschOrMaster,
-    authenticateOwnUser,
-
     buildRequirements
 }
