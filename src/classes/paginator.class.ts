fa0f71e6dd52611ea408930356c4018ca9002ce1
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-26 16:02:52 -0300

    fix: paginator now responds with a BAD REQUEST code in case pagination arguments are not provided in the request.

--- src/classes/paginator.class.ts
+++ src/classes/paginator.class.ts
@@ -1,9 +1,15 @@
+import { AppError } from "../errors";
+
 export default class Paginator<TData> {
     paginatedData: TData[];
     page: number;
     totalPages: number;
 
     public constructor(data: TData[], page: number, pageSize: number) {
+        if (!page || !pageSize) {
+            throw new AppError("Pagination arguments required as query arguments.", 400);
+        }
+
         this.page = page;
 
         const startIndex = (this.page - 1) * pageSize;
