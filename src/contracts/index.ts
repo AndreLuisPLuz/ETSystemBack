004bf68061504545ff47de9ec53dcdb56c577d97
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-28 15:46:12 -0300

    style: added semicolons at /contracts imports

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,11 +1,11 @@
-import { IUserCreatePayload, IUserRegisterPayload } from "./user.interface"
-import { IInstitutionCreatePayload } from "./institution.interface"
-import { IStudentGroupCreatePayload, IStudentGroupUpdatePayload } from "./studentGroup.interface"
-import { IInstructorCreatePayload } from "./instructor.interface"
-import { IAdministratorCreatePayload } from "./administrator.interface"
-import { ILoginPayload, ILoginResponse } from "./login.interface"
+import { IUserCreatePayload, IUserRegisterPayload } from "./user.interface";
+import { IInstitutionCreatePayload } from "./institution.interface";
+import { IStudentGroupCreatePayload, IStudentGroupUpdatePayload } from "./studentGroup.interface";
+import { IInstructorCreatePayload } from "./instructor.interface";
+import { IAdministratorCreatePayload } from "./administrator.interface";
+import { ILoginPayload, ILoginResponse } from "./login.interface";
 import { RequirementTypes, IReqRequirements } from "./reqRequirements.interface";
-import { IDisciplineCategoryCreatePayload } from "./disciplineCategory.interface"
+import { IDisciplineCategoryCreatePayload } from "./disciplineCategory.interface";
 
 export {
     IUserCreatePayload, IUserRegisterPayload,
