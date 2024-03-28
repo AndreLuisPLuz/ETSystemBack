6911757eeef9edc2e33624ec8c8c95e02e3f2e2b
AndreLuisPLuz <andreluispluz@gmail.com>
2024-03-28 11:03:56 -0300

    build: json model for data dictionary added

---
+++ docs/Database/data.js
@@ -0,0 +1,35 @@
+// {
+//     table: string,
+//     description: string,
+//     observations: string,
+//     columns: [
+//         {
+//             name: string,
+//             description: string,
+//             datatype: string,
+//             size: string | null,
+//             keyTypes: string[],
+//             notNull: boolean,
+//             default: string | null
+//         }
+//     ]
+// }
+
+export const tables = [
+    {
+        table: "",
+        description: "",
+        observations: "",
+        columns: [
+            {
+                name: "",
+                description: "",
+                datatype:"",
+                size: "",
+                keyTypes: [""],
+                notNull: true,
+                default: ""
+            }
+        ]
+    }
+]
