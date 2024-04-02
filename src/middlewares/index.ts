7c5ccca284f208a9431a7e4d1db35fa073ec70b9
Murylo Saladino <murylosaladino@gmail.com>
2024-04-02 09:20:21 -0300

    imports

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,11 @@
 import { handleError } from "./handleError.middlewares";
 import { authenticateToken } from "./auth.middleware";
 import { buildRequirements } from "./requirements.middleware";
+import { validateBody } from "./validate.middleware";
 
 export {
     handleError,
     authenticateToken,
-    buildRequirements
+    buildRequirements,
+    validateBody,
 }
