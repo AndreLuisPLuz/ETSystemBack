4a8e84635cf4b44447dab2a64a0ed4720dfd2f70
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-02 15:35:01 -0300

    feat: competence group create payload added, and response formats added as DTOs for competences and competence groups.

---
+++ src/classes/dataTransfer/competenceGroup.dto.ts
@@ -0,0 +1,26 @@
+import { CompetenceGroup } from "../../entities";
+import { CompetenceDTO } from "./competence.dto";
+
+export class CompetenceGroupDTO {
+    idCompetenceGroup!: string;
+    idDiscipline!: string;
+    description!: string;
+
+    public constructor(competenceGroup: CompetenceGroup) {
+        this.idCompetenceGroup = competenceGroup.idCompetence;
+        this.idDiscipline = competenceGroup.discipline.idDiscipline;
+        this.description = competenceGroup.name;
+    }
+};
+
+export class CompetenceGroupSingleDTO extends CompetenceGroupDTO {
+    competences!: CompetenceDTO[];
+
+    public constructor(competenceGroup: CompetenceGroup) {
+        super(competenceGroup);
+
+        this.competences = competenceGroup.competences.map((competence) => 
+            new CompetenceDTO(competence)
+        );
+    }
+}
