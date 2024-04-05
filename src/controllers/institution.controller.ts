759977dc059a809d97be9c3d795768f622205f45
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-05 14:32:20 -0300

    style: long lists or parameters on function signatures changed to fit standard style in institution.controller.

--- src/controllers/institution.controller.ts
+++ src/controllers/institution.controller.ts
@@ -6,22 +6,28 @@ import {
     softDeleteInstitutionService
 } from "../services";
 
-const listIntitutionsController = async(req: Request, res: Response):
-        Promise<Response> => {
+const listIntitutionsController = async(
+    req: Request,
+    res: Response)
+: Promise<Response> => {
     const institutions = await listIntitutionsService(
         Boolean(req.query.is_bosch)
     );
     return res.status(200).json(institutions);
 };
 
-const createInstitutionController = async(req: Request, res: Response):
-        Promise<Response> => {
+const createInstitutionController = async(
+    req: Request,
+    res: Response
+): Promise<Response> => {
     const institution = await createInstitutionService(req.body);
     return res.status(201).json(institution);
 };
 
-const updateInstitutionController = async(req: Request, res: Response):
-        Promise<Response> => {
+const updateInstitutionController = async(
+    req: Request,
+    res: Response
+): Promise<Response> => {
     const institution = await updateInstitutionService(
         req.params.idInstitution,
         req.body
@@ -29,8 +35,10 @@ const updateInstitutionController = async(req: Request, res: Response):
     return res.status(200).json(institution);
 };
 
-const softDeleteInstitutionController = async(req: Request, res: Response):
-        Promise<Response> => {
+const softDeleteInstitutionController = async(
+    req: Request,
+    res: Response)
+: Promise<Response> => {
     await softDeleteInstitutionService(req.params.idInstitution);
     return res.status(204).json();
 };
