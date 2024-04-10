da0e160a2cc9ec7248c07bc9923a538cfc19e589
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-10 13:41:07 -0300

    feat: created controller to access list instructors service.

--- src/controllers/instructor.controller.ts
+++ src/controllers/instructor.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
-import { createInstructorService } from "../services";
+import { listInstructorsService, createInstructorService } from "../services";
+
+const listInstructorsController = async(req: Request, res: Response): Promise<Response> => {
+    const instructors = await listInstructorsService(res.locals.isBosch);
+    return res.status(200).json(instructors);
+}
 
 const createInstructorController = async(req: Request, res: Response): Promise<Response> => {
     const user = await createInstructorService(req.params.idUser);
     return res.status(201).json(user);
 };
 
-export { createInstructorController };
+export {
+    listInstructorsController,
+    createInstructorController
+};
