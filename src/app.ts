4d75071464cfb501f92827962f0ab0e1cfed0779
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-10 13:50:14 -0300

    fix: changed from wrong router object to instructor object for instructor routes.

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.use("/appliedDiscipline", appliedDisciplineRouter);
 app.use("/competenceGroup", competenceGroupRouter);
 app.use("/competence", competenceRouter);
 app.use("/studentAvaliation", studentAvaliationRouter);
-app.use("/instructor", institutionRouter);
+app.use("/instructor", instructorsRouter);
 
 app.use(handleError);
 
